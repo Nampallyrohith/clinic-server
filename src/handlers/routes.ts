625afb30b1786afd4bb19ece5ac69db8ea2f97fb
ManikantaPinabakala <manikantapinabakala@gmail.com>
2025-06-26 14:48:27 +0530

    corrected spelling of a route

--- src/handlers/routes.ts
+++ src/handlers/routes.ts
@@ -218,7 +218,7 @@ router.get(
 
 // patient details
 router.get(
-  "/patient-detials/:patientId",
+  "/patient-details/:patientId",
   defineRoute(async (req, res) => {
     const { patientId } = req.params;
 
